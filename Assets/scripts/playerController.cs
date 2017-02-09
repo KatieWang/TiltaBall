a9b6006284ca32a5dc496a3f03917172f9db2460
KatieWang <katiewang26@gmail.com>
2017-02-08 23:29:09 -0500

    all my files

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            rb.useGravity = false;
+        }
+    }
+}
